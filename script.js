2eb43f47f90113f81c679956e28123b6820c1efc
machi520 <66148552+machi520@users.noreply.github.com>
2020-06-19 13:51:15 +0900

    add @media(max-width:768px)&jQuery
    cssに@media(max-width:768px)を追記。またjQueryとjsファイルを追加。

---
+++ script.js
@@ -0,0 +1,25 @@
+// 画面サイズを取得
+const ScreenWidth = parent.screen.width;
+// widthが768pxより小さかったら、サイドバーを隠す
+console.log(ScreenWidth);
+
+if (ScreenWidth < 768) {
+    $(function(){
+        $('#side-bar').hide();
+    });
+}
+
+// トグルボタンが押されたら
+$(function(){
+    $('#toggle').click(function () {
+        // スライドトグルの動きをして、side-barの表示非表示を切り替える
+        $('#side-bar').slideToggle();
+    });
+});
+
+// サイドのメニューボタンが押されたら、トグルを閉じる
+$(function(){
+    $('.side-menu').click(function () {
+        $('#side-bar').hide();
+    });
+});
